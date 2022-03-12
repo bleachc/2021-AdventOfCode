06e476d66a42418daae98c39a1173d906e721ee1
Ben Leach <ben.leach@enosix.com>
2022-03-12 01:01:27 -0500

    Day 01

---
+++ day01/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+
+let lines =
+    File.ReadLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources/data.txt"))
+    |> Seq.cast<string>
+    |> Seq.map Int32.Parse
+
+let partOne =
+    lines
+    |> Seq.windowed 2
+    |> Seq.map (fun x -> x[0] < x[1])
+    |> Seq.filter id
+    |> Seq.length
+    
+printfn $"{partOne}"
+
+let partTwo =
+    lines
+    |> Seq.windowed 3
+    |> Seq.map Seq.sum
+    |> Seq.windowed 2
+    |> Seq.map (fun x -> x[0] < x[1])
+    |> Seq.filter id
+    |> Seq.length
+
+printfn $"{partTwo}"
